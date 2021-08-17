ac410ec1e49e31401d5087c2e2594684222b47fc
Grupo03 - LuisPiovan <ex150351@g.unicamp.br>
2021-08-17 13:30:08 -0300

    Finishing Test Automation with Jasmine and Karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { AppModule } from './app.module';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let appComponent: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppModule],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    appComponent = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should be created the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(appComponent).toBeTruthy();
+  });
+
+  it(`#${AppComponent.prototype.like.name} should trigger liked() when called`, () => {
+    appComponent.like();
+    expect(appComponent.likes).toEqual(appComponent.likes);
   });
 });
